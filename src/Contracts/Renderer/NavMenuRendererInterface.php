82220b651a65df5be24a2ace705a0830a1b727cd
vigihdev <vigihdev@email.com>
2026-05-28 16:12:09 +0700

    💥 2026-05-28 16:12:09

--- src/Contracts/Renderer/NavMenuRendererInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace VigihdevWP\Widgets\Contracts\Renderer;
-
-use VigihdevWP\Widgets\Contracts\RenderableInterface;
-
-interface NavMenuRendererInterface extends RenderableInterface {}
